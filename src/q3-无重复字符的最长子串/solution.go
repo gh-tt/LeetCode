21c173d307cacd7d70522c37534702982f1931d9
ghtt <519840013@qq.com>
2020-06-02 17:27:58 +0800

    q3 无重复字符的最长子串

---
+++ src/q3-无重复字符的最长子串/solution.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func main() {
+	s := "abccbaadbbef"
+	length := lengthOfLongestSubString(s)
+	fmt.Println(length)
+}
+
+func lengthOfLongestSubString(s string) int {
+	m := make([]int,128)
+
+	n := len(s)
+
+	j, re := 0, 0
+
+	for i := 0; i < n; i++ {
+		if i != 0 {
+			m[s[i-1]]--
+		}
+
+		for j < n && m[s[j]] == 0 {
+			m[s[j]]++
+			j++
+		}
+
+		re = max(re, j-i)
+	}
+
+	return re
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+
+	return y
+}
