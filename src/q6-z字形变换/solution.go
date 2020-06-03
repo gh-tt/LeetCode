6df0b561091e0087ab2e166070e5b4d4f69b25ec
ghtt <519840013@qq.com>
2020-06-03 17:26:52 +0800

    q6 z字形变换

---
+++ src/q6-z字形变换/solution.go
@@ -0,0 +1,24 @@
+package main
+
+func main() {
+
+}
+
+func convert(s string, numRows int) string {
+	if numRows < 2 {
+		return s
+	}
+
+	var tmp = make([]string, numRows)
+
+	curr := 0
+	flag := -1
+
+	for _, val := range s {
+		tmp[curr] += string(val)
+
+		if  {
+			
+		}
+	}
+}
