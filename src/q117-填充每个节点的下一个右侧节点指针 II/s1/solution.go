60dba26f415a32d7c08ea730102680cedfea10fd
ghtt <519840013@qq.com>
2020-06-20 17:19:32 +0800

    q117 填充每个节点的下一个右侧节点指针 II

---
+++ src/q117-填充每个节点的下一个右侧节点指针 II/s1/solution.go
@@ -0,0 +1,32 @@
+package main
+
+type Node struct {
+	Val   int
+	Left  *Node
+	Right *Node
+	Next  *Node
+}
+
+func connect(root *Node) *Node {
+	if root == nil {
+		return nil
+	}
+	queue := []*Node{root}
+	for len(queue) > 0 {
+		l := len(queue)
+		for i := 0; i < l; i++ {
+			node := queue[0]
+			queue = queue[1:]
+			if i < l-1 {
+				node.Next = queue[0]
+			}
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+	}
+	return root
+}
