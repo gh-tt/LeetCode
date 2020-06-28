b7244179102f02d1900e1f8fd83674480e10e7ff
ghtt <519840013@qq.com>
2020-06-28 15:07:09 +0800

    q226 翻转二叉树

---
+++ src/q226-翻转二叉树/s1/solution.go
@@ -0,0 +1,28 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return root
+	}
+	queue := []*TreeNode{root}
+	for len(queue) > 0 {
+		curr := queue[len(queue)-1]
+		queue = queue[:len(queue)-1]
+		tmp := curr.Left
+		curr.Left = curr.Right
+		curr.Right = tmp
+		if curr.Left != nil {
+			queue = append(queue, curr.Left)
+		}
+		if curr.Right != nil {
+			queue = append(queue, curr.Right)
+		}
+	}
+	return root
+}
